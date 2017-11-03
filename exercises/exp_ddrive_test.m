b08686472f9edd7de52ba38b5bfbc62308e5e4da
PabloRdrRbl <pablordrrbl@gmail.com>
2017-11-03 23:20:26 +0100

    Add solutions first exercise

--- exercises/exp_ddrive_test.m
+++ exercises/exp_ddrive_test.m
@@ -11,18 +11,30 @@ function exp = exp_ddrive_test(varargin)
     % Add an environment map (serves as a background image)
     exp.environment = grp_obstacles_and_landmarks_from_image('../maps/emptyroom.png', 'scale', 0.01);
     
-    % Instantiate the robot
+    % Instantiate the first robot
     exp.robot.platform = model_platform2d_ddrive([2 0.75 90*pi/180]);
     %exp.robot.platform.wheelRadius = [0.030 0.0303];    
-    %exp.robot.platform.wheelDistance = 0.255;
+    exp.robot.platform.wheelDistance = 0.255;
     exp.robot.radius = 0.14;
     exp.robot.color = [0 0 1];
     exp.robot.wheelRadius = 0.03;
-    exp.robot.wheelDistance = 0.25;    
+    exp.robot.wheelDistance = 0.25;   
+    
+    % Add range finder 
+    exp.robot.sensor = sensor_rangefinder2d();
     
     % add a sink to convince the simulator engine, that the robot is used.
     % Otherwise the engine would 'optimize' the robot away
     exp.robot.sink = block_sink('platform');
+    
+    % Instantiate the second robot 
+    exp.robot2.platform = model_platform2d_omni([2 2.5 90*pi/180]);
+    exp.robot2.radius = 0.14;
+    exp.robot2.color = [0.5 0 1]; % Farbe setzen 
+    exp.robot2.sink = block_sink('platform'); 
+    exp.robot2.controller = guidance_omni_circle(); 
+    exp.robot2.controller.radius = 1.5; 
+    exp.robot2.controller.rotVelocity = 20*pi/180;
     
     % If you have a gamepad or joystick at hand you can steer your robot
     % interactively by setting useGamepad to true
@@ -48,6 +60,7 @@ function exp = exp_ddrive_test(varargin)
     % robot. Therefore we had to add the sink above, which is discrete and
     % can be put into the depends list)
 	exp.depends = {'*sink', '*environment*'};
+    %exp.depends{end + 1} = '*sensor';
     
     % Some parameters that influence the appereance of the display area
     exp.display.settings = {'XGrid', 'on', 'YGrid', 'on', 'Layer', 'top', 'XLim', [0 8], 'YLim', [0 6]};
