378755b70848de5d8f826a51a33a3b743225ba69
PabloRdrRbl <pablordrrbl@gmail.com>
2017-12-28 21:41:12 +0100

    Added Exercise 2

--- project/WGSpos2ECEFpos.m
+++ project/WGSpos2ECEFpos.m
@@ -1,10 +1,19 @@
 function rECEF = WGSpos2ECEFpos(rWGS)
-    a = 6378137;
-    e = 0.081819;
+    %    
+    % rWGS: (3,1) vector containing coordinates in WGS coordinates
+    %       (phi, lambda, h)'
     
-    % TODO: convert a position from WGS84 to ECEF representation
+    a = 6378137;  % (m) Earth radius
+    e = 0.081819;  % (-) Earth orbit eccentricity
+    
+    % Conversion of a position from WGS84 to ECEF representation
 
     rECEF = zeros(3, 1);
+    
+    % See slide 6 from "Hilfsblatt"
+    rECEF(1) = (a + rWGS(3)) * cos(rWGS(1)) * cos(rWGS(2));
+    rECEF(2) = (a + rWGS(3)) * cos(rWGS(1)) * sin(rWGS(2));
+    rECEF(3) = (a * (1 - e^2) + rWGS(3)) * sin(rWGS(1));
 
 end
 
--- project/WGSpos2NEDquat.m
+++ project/WGSpos2NEDquat.m
@@ -1,8 +1,23 @@
 function NEDquat = WGSpos2NEDquat(WGSpos)
+    %    
+    % rWGS: (3,1) vector containing coordinates in WGS coordinates
+    %       (phi, lambda, h)'
 
-    % TODO: Compute the Attitude of an NED frame of which the origin is
+    % Abbreviations
+    sp = sin(WGSpos(1));  % sin of phi
+    cp = cos(WGSpos(1));  % cos of phi
+    sl = sin(WGSpos(2));  % sin of lambda
+    cl = cos(WGSpos(2));  % cos of lambda
+   
+    % Attitude of an NED frame of which the origin is
     % located at the given WGS84 position
+    
     R = eye(3);
+    
+    % See slide 8 from "Hilfsblatt"
+    R = [-sp*cl, -sl, -cp*cl;
+         -sp*sl,  cl, -cp*sl;
+              cp,  0,    -sp];
     
     NEDquat = DCM2Quat(R);
 
